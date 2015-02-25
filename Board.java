535c37e188cb3c079c2567ac6cc455bff1476d21
thebareju <noahbweiner@gmail.com>
2015-02-25 11:47:56 -0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -20,7 +20,20 @@ public class Board {
   }
 
   public String drawBoard() {
-    String output = "";
+String output = "";
+
+    // number the spaces
+    for (int i = 0; i < board.length; i++) {
+      // to handle spacing of both single- and double-digit numbers
+      if (i < 9) {
+        output += " ";
+      }
+
+      output += (i + 1) + " ";     // Should result in: " 1  2  3  4 "...etc
+    }
+
+    // add second line to label spaces
+    output += "\n";    // newline character "\n" = drops down a line
 
     // iterate over board
     for (int i = 0; i < board.length; i++) {
@@ -33,13 +46,6 @@ public class Board {
         output += "[ ]";
       }
 
-    }
-
-    // add a second line to label spaces
-    output += "\n\n";    // two newline characters
-
-    for (int i = 0; i < board.length; i++) {
-      output += " " + (i + 1) + " ";     // Should result in: " 1  2  3  4 "...etc
     }
 
     return output;
