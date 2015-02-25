91767fb13740b2a1274545108fc089bd24489293
thebareju <noahbweiner@gmail.com>
2015-02-25 11:31:33 -0800

    first commit

---
+++ Board.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class Board {
+  private boolean[] board;
+
+  public Board() {
+
+  }
+
+  public void setupBoard(int coins) {
+    board = new boolean[10];
+    Arrays.fill(board, false);
+    board[1] = true;
+    board[4] = true;
+    board[7] = true;
+    board[8] = true;
+  }
+
+  public boolean moveCoin(int location) {
+    return true;
+  }
+
+  public boolean gameOver() {
+    return false;
+  }
+
+  public String drawBoard() {
+    String output = "";
+
+    // iterate over board
+    for (int i = 0; i < board.length; i++) {
+
+      if (board[i] == true) {
+        // there is a coin here
+        output += "[0]";
+      } else {
+        // blank space
+        output += "[ ]";
+      }
+
+    }
+
+    // add a second line to label spaces
+
+    output += "\n\n";    // two newline characters
+
+    for (int i = 0; i < board.length; i++) {
+      output += " " + (i + 1) + " ";     // Should result in: " 1  2  3  4 "...etc
+    }
+
+    return output;
+  }
+
+  public static void main(String[] args) {
+    Board b = new Board();
+    b.setupBoard(0);
+    System.out.println(b.drawBoard());
+
+    return;
+  }
+}
